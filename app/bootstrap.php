20a031d3fdf81094caf949c8d46effbc496995c4
mohamedelaraby <mohamedelaraby1995@gmail.com>
2019-10-29 18:07:05 +0200

    Setup bootstrap files

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,1 +1,15 @@
-<h2>hello</h2>
+<?php 
+/*
+[  author ] ;- Muhammed alaraby
+[  Date   ] ;- 29/10/19
+[  Info   ] ;- { Bootstrap file } :-  Load Important Core files  
+*/
+
+////////////////////////////////////////////////////////////////////////
+
+ // Load Libraries
+    require_once 'libraries/Core.php';
+    require_once 'libraries/Database.php';
+    require_once 'libraries/Controller.php'
+
+?>
