c9632976714e551b40671efb45d5d3c98f9b8c5f
mohamedelaraby <mohamedelaraby1995@gmail.com>
2019-12-12 06:33:42 +0200

    Base Controller class

--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -2,8 +2,48 @@
 /*
 [  author ] ;- Muhammed alaraby
 [  Date   ] ;- 29/10/19
-[  Info   ] ;- { Controller } :- Handle contetn between UI and Backend
+[  Info   ] ;- Base controller which loads the models and views
 */
 
 
+class Controller {
+
+    // [ model ] :- Return the required model for the current controller
+    // [ $model ]:-  Model file
+    public function model($model){
+        //Require model file
+        require_once '../app/models/'.$model.'.php';
+        // Instatiate model
+        return new $model();
+    }
+
+    // [ view ] :- Return views
+    // [ $view ] :- View file
+    // [ $data = []] :- Handle  dynamic data into the view
+    public function view($view, $data = []){
+        // Check for view file
+        if(file_exists('../app/views/'.$view/'.php')){
+            require_once '../app/views/'.$view/'.php';
+        } else {
+            die('View does not exist');
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
+
